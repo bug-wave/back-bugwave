ecab3ee6fbe22370c0dfb2a04b709e8f88d257c3
Lucas Lessa <lucasleitelessa@gmail.com>
2025-05-07 20:56:51 -0300

    build: dependencies

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
